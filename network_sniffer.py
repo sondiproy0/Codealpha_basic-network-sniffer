7129195c0b1d550d4b9e6ac90db3228deca098da
Sondip Roy <58737865+sondiproy0@users.noreply.github.com>
2024-04-29 23:46:20 +0600

    Update network_sniffer.py

--- network_sniffer.py
+++ network_sniffer.py
@@ -1,10 +1,11 @@
 import scapy.all as scapy
+import time
 
 def sniffing(interface):
-	scapy.sniff(iface=interface,store=False,prn=process_packet)
+    scapy.sniff(iface=interface, store=False, prn=process_packet)
 
 def process_packet(packet):
-	print(packet)
-
+    print(packet)
+    time.sleep(0.5)
 
 sniffing('Ethernet')
